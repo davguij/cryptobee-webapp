32f8d9c68ae89926ebb8bf53612745cc8283165a
David Guijarro <guijarro.dav@gmail.com>
2017-07-16 19:48:18 +0200

    Working on Home view.

---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-layout wrap>
+      <v-flex xs12 sm6>
+        <v-card>
+          <v-card-title>
+            Your balance:
+          </v-card-title>
+          <v-card-text>
+            <p class="display-4">$12,234</p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <v-flex xs4 sm2>
+        <coin-detail coin="btc"></coin-detail>
+      </v-flex>
+      <v-flex xs4 sm2>
+        <coin-detail coin="ltc"></coin-detail>
+      </v-flex>
+      <v-flex xs4 sm2>
+        <coin-detail coin="eth"></coin-detail>
+      </v-flex>
+    </v-layout>
+    <v-card>
+      <v-card-title>
+        Graph
+      </v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import CoinDetail from './home-widgets/CoinDetail';
+
+export default {
+  name: 'home',
+  components: {
+    'coin-detail': CoinDetail,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ src/components/home-widgets/CoinDetail.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-card>
+    <v-card-title>
+      {{coin.toUpperCase()}}
+    </v-card-title>
+    <v-card-text>
+      <p>
+        <strong>Total balance:</strong>
+        <br>23 {{coin.toUpperCase()}}
+        <br>$25</p>
+      <p>
+        <strong>Exchange rate:</strong>
+        <br>$2,342</p>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'coin-detail',
+  props: ['coin'],
+};
+</script>
+
+<style>
+
+</style>
