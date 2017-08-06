3709797c895f9080152a35264d2b925339cb029b
David Guijarro <guijarro.dav@gmail.com>
2017-08-06 18:56:33 +0200

    Bye burger menu.

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,40 @@
   <div>
     <nav class="navbar">
       <div class="navbar-brand">
-        <a class="navbar-item" href="http://bulma.io">
-          <img src="./assets/cryptobee-logo.jpg" class="nav-logo" alt="Bulma: a modern CSS framework based on Flexbox">
-        </a>
-  
-        <div class="navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
+        <div class="navbar-item">
+          <img src="./assets/cryptobee-logo.jpg" class="nav-logo" alt="Cryptobee">
         </div>
+        <router-link class="navbar-item is-hidden-desktop" to="/">Dashboard</router-link>
+        <router-link class="navbar-item is-hidden-desktop" to="/address-book">Address book</router-link>
       </div>
       <div class="navbar-menu">
         <div class="navbar-start">
         </div>
         <div class="navbar-end">
-          <router-link class="navbar-item" to="/">Dashboard</router-link>  
-          <router-link class="navbar-item" to="/address-book">Address book</router-link>  
+          <router-link class="navbar-item" to="/">Dashboard</router-link>
+          <router-link class="navbar-item" to="/address-book">Address book</router-link>
           </a>
         </div>
       </div>
     </nav>
     <router-view></router-view>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Bulma</strong> by
+            <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed
+            <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+          </p>
+          <p>
+            <a class="icon" href="https://github.com/jgthms/bulma">
+              <i class="fa fa-github"></i>
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>  
   </div>
 </template>
 
@@ -40,8 +53,8 @@ export default {
 </script>
 
 <style>
-  img.nav-logo {
-    max-height: 3.5rem;
-    margin-bottom: -0.65rem;
-  }
+img.nav-logo {
+  max-height: 3.5rem;
+  margin-bottom: -0.65rem;
+}
 </style>
