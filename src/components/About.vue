e986cc3ea5c53d6ac350e0f78d345c62b92ca64a
David Guijarro <guijarro.dav@gmail.com>
2017-08-19 12:29:47 +0200

    First content for About page.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,20 @@
 <template>
   <section class="section">
-    About
+    <div class="container">
+      <h1 class="title">What's this thing all about?</h1>
+      <h2 class="subtitle">
+        Find it hard to keep track of your cryptocoin portfolio value?
+      </h2>
+      <p>
+        <strong>cryptobee</strong> allows you to add your crypto wallets and get an overview of your current accumulated balance in fiat. The app runs without any registration and your wallets are saved locally in your device. These measures improve your privacy, as they make it hard for anybody (including this app's developer) to keep track of which wallets belong to each user.
+      </p>
+      <h2 class="subtitle">
+        
+      </h2>
+      <p>
+        Currently, US$ is the only supported fiat currency. As for cryptocurrencies, we support Bitcoin, Litecoin, Ethereum, Dogecoin and Dash.
+      </p>
+    </div>
   </section>
 </template>
 
